7839250b7b124124b16d5e9f79c504db1ea0bbe4
SaRa <119528406+SaRa126v@users.noreply.github.com>
2023-07-17 01:07:41 +0330

    update
    nearly fixed all bugs
    added disable or enable button
    eye svg

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ inputs.forEach((input) => {
     e.target.parentElement.classList.add("activeInput");
 
     // add legend
-    input.parentElement.firstElementChild.style.display = "block";
+    input.parentElement.firstElementChild.classList.add("legendActive");
+    console.log(input.parentElement.firstElementChild);
 
     // remove svg
     if (input.previousElementSibling.classList.contains("firstSvg")) {
@@ -25,8 +26,7 @@ inputs.forEach((input) => {
     e.target.parentElement.classList.remove("activeInput");
 
     // remove legend
-    input.parentElement.firstElementChild.style.display = "none";
-
+    input.parentElement.firstElementChild.classList.remove("legendActive");
     // add svg
     input.previousElementSibling.style.display = "block";
 
@@ -34,29 +34,17 @@ inputs.forEach((input) => {
     input.classList.remove("placeholderInput");
 
     // calling checker functions
-    usernameChecker();
-    emailChecker();
-    passwordChecker();
-    confirmPasswordChecker();
-  });
-
-  // onMouseMove..................................
-  input.addEventListener("mousemove", (e) => {
-    // if active >> remove hover
-    // if not >> add hover
-    if (
-      e.target.parentElement.classList.contains("activeInput") ||
-      e.target.parentElement.classList.contains("error") ||
-      e.target.parentElement.classList.contains("success")
-    ) {
-      e.target.parentElement.classList.remove("hoverInput");
-    } else {
-      e.target.parentElement.classList.add("hoverInput");
+    if (input.id === "username") {
+      usernameChecker();
+    } else if (input.id === "email") {
+      emailChecker();
+    } else if (input.id === "password") {
+      passwordChecker();
+    } else if (input.id === "confirmPassword") {
+      confirmPasswordChecker();
     }
   });
-
 });
-
 
 // getting form values without space..............................................
 
@@ -67,7 +55,6 @@ let email = document.querySelector("#email");
 let password = document.querySelector("#password");
 
 let confirmPassword = document.querySelector("#confirmPassword");
-
 
 // condition for the values........................
 // for checking username...........................
@@ -93,11 +80,9 @@ function usernameChecker() {
 
     // if it filled and valid
   } else {
-    usernameMessege = "";
     success(username, usernameMessege);
   }
 }
-
 
 // for checking email.............................
 function emailChecker() {
@@ -105,20 +90,18 @@ function emailChecker() {
   let emailValue = email.value.trim();
   // a variable for messeges
   let emailMessege;
-  // if it is blank give error
-  if (emailValue == "") {
+
+  // if it is valid by using Regex (regular expressions)
+  if (emailValue.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    success(email, emailMessege);
+
+    // if it is empty
+  } else if (emailValue == "") {
     // messege for empty email field
     emailMessege = "Oops! Looks like you missed a field.";
 
     // call error function to make it all red
     error(email, emailMessege);
-
-    // if it is valid by using Regex (regular expressions)
-  } else if (
-    emailValue.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)
-  ) {
-    emailMessege = "";
-    success(email, emailMessege);
 
     // invalid email
   } else {
@@ -129,25 +112,27 @@ function emailChecker() {
   }
 }
 
-
 // for checking password...........................
 function passwordChecker() {
   // getting input value & removing space
   let passwordValue = password.value.trim();
   // a variable for messeges
   let passwordMessege;
-  // if it is blank give error
-  if (passwordValue == "") {
+
+  // if password is strong
+  if (
+    passwordValue.match(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,}$/
+    )
+  ) {
+    success(password, passwordMessege);
+    // if it is blank give error
+  } else if (passwordValue == "") {
     // messege for empty password field
     passwordMessege = "Ah, it seems like we're missing something here.";
 
     // call error function to make it all red
     error(password, passwordMessege);
-
-    // if password is strong
-  } else if (passwordValue.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,}$/)) {
-    passwordMessege = "";
-    success(password, passwordMessege)
 
     // if password is too short
   } else if (passwordValue.length < 8) {
@@ -155,16 +140,16 @@ function passwordChecker() {
 
     // call error function to make it all red
     error(password, passwordMessege);
-  
+
     // if password is weak
   } else {
-    passwordMessege = "Please use a combination of letters, numbers, and symbols in your password.";
+    passwordMessege =
+      "Please use a combination of letters, numbers, and symbols in your password.";
 
     // call error function to make it all red
     error(password, passwordMessege);
-    }
   }
-
+}
 
 // for checking confirmPassword...................
 function confirmPasswordChecker() {
@@ -173,27 +158,23 @@ function confirmPasswordChecker() {
   // a variable for messeges
   let confirmPasswordMessege;
   // if it is blank give error
+
   if (confirmPasswordValue == "") {
     // messege for empty confirmPassword field
     confirmPasswordMessege = "Just fill it in and we'll be on our way!";
 
     // call error function to make it all red
     error(confirmPassword, confirmPasswordMessege);
-
   } else if (confirmPasswordValue === password.value) {
-
-    confirmPasswordMessege = "";
+    // confirmPasswordMessege = "";
     success(confirmPassword, confirmPasswordMessege);
+  } else {
+    confirmPasswordMessege = "Oh snap! Your passwords don't match";
 
-    } else {
-      confirmPasswordMessege = "Oh snap! Your passwords don't match";
-
-      // call error function to make it all red
-      error(confirmPassword, confirmPasswordMessege);
-
-    }
+    // call error function to make it all red
+    error(confirmPassword, confirmPasswordMessege);
   }
-
+}
 
 // error(red) and success(green) function..........
 
@@ -208,11 +189,18 @@ function error(input, messege) {
 }
 
 // for success
-function success(input, messege) {
+function success(input) {
+  // remove error class if there is any
+  if (input.parentElement.classList.contains("error")) {
+    input.parentElement.classList.remove("error");
+  }
+
   // removing the messege in html
-  input.parentElement.nextElementSibling.innerHTML = messege;
+  input.parentElement.nextElementSibling.innerHTML = "";
+
   // adding green border
   input.parentElement.classList.add("success");
+
   // green svg
   input.previousElementSibling.style.fill = "#04aa6d";
 }
@@ -220,24 +208,45 @@ function success(input, messege) {
 // ***************************************
 // show password when secondSvg is clicked.........
 // remove & add another svg
-let secondSvg = document.querySelector(".secondSvg");
+let secondSvgs = document.querySelectorAll(".secondSvg");
 
-let showPas = document.querySelector(".showPas");
+secondSvgs.forEach((svg) => {
+    svg.addEventListener("click", (e) => {
+        const targetInput = e.target.previousElementSibling;
 
-secondSvg.addEventListener("click", () => {
-  if (showPas.type === "password") {
-    showPas.type = "text";
-  }
+        if (targetInput.getAttribute("type") === "password") {
+            targetInput.setAttribute("type", "text");
+        } else {
+            targetInput.setAttribute("type", "password");
+        }
+    });
+});
 
-  });
+
+// get attribute ro nemikhune vagti attributi vojud nadare****************************
 
 // send form when button is clicked................
-// if all of them have success class remove disabled from button 
+
+// if all of them have success class remove disabled from button
 
 let button = document.querySelector("button");
 
-button.addEventListener("click", (e) => {
-e.preventDefault();
-
+button.addEventListener("click", () => {
+  inputs.forEach((input) => {
+    if (input.parentElement.classList.contains("success")) {
+      button.disabled = false;
+      console.log("it is false");
+      console.log(button);
+    } else {
+      button.disabled = true;
+      console.log("it is true");
+      console.log(button);
+      console.log(input.parentElement);
+    }
+  });
 });
 
+// for testing:
+// sara
+// Sara@gmail.com
+// Sara12345$
